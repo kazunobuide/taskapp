bc5e621072a91811ae9d932b6017162115b82e03
kazunobu.ide <idekazunobu@gmail.com>
2020-12-27 19:20:55 +0900

    エラーが出るが一旦コーディング

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -7,15 +7,88 @@
 //
 
 import UIKit
+import RealmSwift
+import UserNotifications
 
 class InputViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    let realm = try! Realm()//追加した
+    var task: Task!//←追加した
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    //背景をタップしたらdismissKeyboardメソッドを呼ぶように設定
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+    }
+    
+    override func viewWillDisappear(_ animated: Bool){
+        try! realm.write{
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: .modified)
+        }
+        
+        setNotification(task: task)//追加
+        
+        super.viewWillDisappear(animated)
+    }
+        
+    //タスクのローカル通知を登録する
+    func setNotification(task: Task){
+        let content = UNMutableNotificationContent()
+        //タイトルと内容を設定（中身がない場合メッセージ無しで音だけの通知になるので「（××なし）」を表示する）
+        if task.title == ""{
+            content.title = "(タイトルなし)"
+        }else{
+            content.title = task.title
+        }
+        if task.contents == ""{
+            content.body = "(内容なし)"
+        }else{
+            content.body = task.contents
+        }
+        content.sound = UNNotificationSound.default
+        
+        //ローカル通知が発動するtrigger(日付マッチ)を作成
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from : task.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        //idntifier, content, triggerからローカル通知を作成(identifierが同じだとローカル通知を上書き保存)
+        let request = UNNotificationRequest(identifier: String(task.id), content: content, trigger: trigger)
+        
+        //ローカル通知を記録
+        let center = UNUserNotificationCenter.current()
+        center.add(request){(error) in
+            print(error ?? "ローカル通知記録 OK")//errorがnilならローカル通知の登録に成功したと表示します。errorが存在すればerrorを表示します。
+            
+        //未通知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests { (request: [UNNotificationRequest]) in
+                for request in request{
+                    print("/-----------")
+                    print(request)
+                    print("-----------/")
+                }
+            }
+        
+        
+        }
+    }
+        
+        @objc func dismissKeyboard(){
+            //キーボードを閉じる
+            view.endEditing(true)
+        }
         // Do any additional setup after loading the view.
     }
     
@@ -29,5 +102,3 @@ class InputViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
-}
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -6,9 +6,19 @@
 //  Copyright © 2020 kazunobu.ide. All rights reserved.
 //
 import UIKit
+import RealmSwift //←追加した
+import UserNotifications//追加
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    
+    //Realmインスタンスを取得する
+    let realm = try! Realm()// ←追加した
+    
+    //DB内のタスクが格納されるリスト。
+    //日付の近い順でソート：昇順
+    //以降内容をアップデートするとリスト内は自動的に更新される
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)//←追加した
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +28,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     // データの数（＝セルの数）を返すメソッド
+    //tableView(_:numberOfRowsInSection:)メソッドはデータの数を返すメソッドなのでデータの配列であるtaskArrayの要素数を返すようにします。
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskArray.count//修正した
     }
 
     // 各セルの内容を返すメソッド
+    //tableView(_:cellForRowAtIndexPath:)メソッドは各セルの内容を返すメソッドです。データの配列であるtaskArrayから該当するデータを取り出してセルに設定します。ここで登場するDateFormatterクラスは日付を表すDateクラスを任意の形の文字列に変換する機能を持ちます。
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 再利用可能な cell を得る
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
+        //Cellに値を設定する。
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
+        
         return cell
     }
 
@@ -42,5 +64,49 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            //削除するタスクを取得する
+            let task = self.taskArray[indexPath.row]
+            
+            //ローカル通知をキャンセルする
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+            //データベースから削除する
+            try! realm.write{
+                self.realm.delete(task)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            //未通知のローカル通知一覧とログ出力
+            center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+                for request in requests{
+                    print("/----------")
+                    print(request)
+                    print("----------/")
+                }
+            }
+        }
+    }
+    //segueで画面遷移する時に呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        if segue.identifier == "cellSegue"{
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        }else{
+            let task = Task()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
+    }
+    //入力画面から戻ってきた時にTableViewを更新させる
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 }
